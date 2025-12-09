34a20d0eab173fc832fbf47b12bc7af4137fcf9a
alalitha014-rgb <alalitha014@gmail.com>
2025-12-09 12:10:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Brightland/',  // must match your repo name
   plugins: [react()],
-  base: '/Brightland/',   // ✅ MUST match your GitHub repo name exactly
 })
